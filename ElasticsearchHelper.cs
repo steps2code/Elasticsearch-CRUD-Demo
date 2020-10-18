f62863adc28ad84d45dbdb8dbc98c09f2e0fbb8b
steps2code <steps2mycode@gmail.com>
2020-10-18 21:37:22 +0530

    Elasticsearch-CRUD-Demo

---
+++ ElasticsearchHelper.cs
@@ -0,0 +1,70 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticsearchCRUDDemo
+{
+    public static class ElasticsearchHelper
+    {
+        public static ElasticClient GetESClient()
+        {
+            ConnectionSettings connectionSettings;
+            ElasticClient elasticClient;
+            StaticConnectionPool connectionPool;
+
+            var nodes = new Uri[] { 
+                new Uri("http://2863edd07334.ngrok.io") //Provide your Elasticsearch endpoint.
+            };
+
+            connectionPool = new StaticConnectionPool(nodes);
+            connectionSettings = new ConnectionSettings(connectionPool);
+            elasticClient = new ElasticClient(connectionSettings);
+
+            return elasticClient;
+        }
+
+        public static void CreateDocument(ElasticClient elasticClient, string indexName, string typeName, Product product, string documentId)
+        {
+            var response = elasticClient.Index(product, i => i
+            .Index(indexName)
+            .Type(typeName)
+            .Id(documentId)
+            .Refresh(Elasticsearch.Net.Refresh.True));
+        }
+
+        public static void GetDocument(ElasticClient elasticClient, string indexName, string typeName, string documentId)
+        {
+            var response = elasticClient.Search<Product>(s => s
+              .Index(indexName)
+              .Type(typeName)
+              .Query(q => q.Term(t => t.Field("_id").Value(documentId))));
+            foreach (var hit in response.Hits)
+            {
+                Console.WriteLine("Id:{0} Name:{1} Description:{2}", hit.Source.id, hit.Source.name, hit.Source.description);
+            }
+
+        }
+
+        public static void UpdateDocument(ElasticClient elasticClient, string indexName, string typeName, Product product, string documentId)
+        {
+            var response = elasticClient.Index(product, i => i
+                  .Index(indexName)
+                  .Type(typeName)
+                  .Id(documentId)
+                  .Refresh(Elasticsearch.Net.Refresh.True));
+        }
+
+        public static void DeleteDocument(ElasticClient elasticClient, string indexName, string typeName, string documentId)
+        {
+            var response = elasticClient.Delete<Product>(documentId, d => d
+           .Index(indexName)
+           .Type(typeName));
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Nest;
+using System;
+
+namespace ElasticsearchCRUDDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string INDEX_NAME = "newtestdb";
+            string TYPE_NAME = "product";
+            string documentId = "1";
+            Product product = new Product
+            {
+                id = documentId,
+                name = "Samsung TV",
+                category = "LCD TV",
+                description = "Samsung LCD 100 inch TV",
+                isPopularProduct = 1,
+                price = 50000,
+                keyWords = "Tv,LCD,SAMSUNG TV"
+            };
+
+            //1.Connect to Elastic Search.
+            ElasticClient elasticClient = ElasticsearchHelper.GetESClient();
+
+            //2.Creating Documents In Elasticsearch.
+             ElasticsearchHelper.CreateDocument(elasticClient, INDEX_NAME,TYPE_NAME, product,documentId);
+
+            //3.Getting Documents From Elasticsearch.
+            ElasticsearchHelper.GetDocument(elasticClient, INDEX_NAME, TYPE_NAME,documentId);
+
+            //4.Updating Documents In Elasticsearch.
+            // ElasticsearchHelper.UpdateDocument(elasticClient, INDEX_NAME, TYPE_NAME, product, documentId);
+
+            //5.Deleting Documents From Elasticsearch.
+            ElasticsearchHelper.DeleteDocument(elasticClient, INDEX_NAME, TYPE_NAME, documentId);
+            Console.ReadKey();
+        }
+    }
+
+}
